57c93eea919eaec6a858144e0c1c8f1c45f31901
Jubayer <zubayerhamim@gmail.com>
2023-04-04 13:40:20 +0600

    add page and route files for app

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,41 @@
+import 'package:get/get.dart';
+
+import '../modules/auth/login/bindings/login_binding.dart';
+import '../modules/auth/login/views/login_view.dart';
+import '../modules/auth/registration/bindings/registration_binding.dart';
+import '../modules/auth/registration/views/registration_view.dart';
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+import '../modules/user/user_dashboard/bindings/user_dashboard_binding.dart';
+import '../modules/user/user_dashboard/views/user_dashboard_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.LOGIN;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.REGISTRATION,
+      page: () => const RegistrationView(),
+      binding: RegistrationBinding(),
+    ),
+    GetPage(
+      name: _Paths.USER_DASHBOARD,
+      page: () => const UserDashboardView(),
+      binding: UserDashboardBinding(),
+    ),
+  ];
+}
