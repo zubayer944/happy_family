e4e03543a82c56c77c35faefda7d5cd99a7cddc7
Jubayer <zubayerhamim@gmail.com>
2023-04-04 13:40:02 +0600

    add all string files for app string

---
+++ lib/app/common/utils/all_strings.dart
@@ -0,0 +1,20 @@
+class AllStrings {
+  // validation
+  static String emailOrPhoneIsRequired = "E-mail or Phone is required";
+  static String dropdownItemIsRequired = "please select an item";
+  static String invalidEmailAddress = "Invalid email address";
+  static String passwordIsRequired = "Password is required";
+  static String atLeast1CharNeeded = "At least 1 character needed";
+  static String roleTypeValidationText = "Only letter , hyphen(-) and underscore(_) accepted";
+  static String atLeast1DigitNeeded = "At least 1 digit needed";
+  static String roleTypeIsRequired = "Role type is required";
+  static String minimum8Char = "minimum 8 characters";
+  static String enterNewPasswordFirst = "Enter new password first";
+  static String newPasswordAndConfirmPasswordNotMatch = "New Password and Confirm Password Doesn't match";
+  static String oldPassAndCurrentPassNotMatching = "Old Password and Current Password Doesn't match";
+  static String mustBeAcceptedOurTermsAndCondition = "Must be accepted our terms and condition";
+  static String fullNameIsRequired = 'Full name is required';
+  static String phoneIsRequired = "Phone is required";
+  static String companyNameIsRequired = "Company name is required";
+  static String designationIsRequired = "Designation is required";
+}
